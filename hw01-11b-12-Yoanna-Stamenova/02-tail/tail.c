5878170df2cea1206f321a831a9e2c5c7e122a57
Yoanna Stamenova <150888420+storaroyoni@users.noreply.github.com>
2023-12-03 15:31:37 +0200

    Update tail.c

--- hw01-11b-12-Yoanna-Stamenova/02-tail/tail.c
+++ hw01-11b-12-Yoanna-Stamenova/02-tail/tail.c
@@ -6,34 +6,50 @@
 #include <fcntl.h>
 #include <errno.h>
 
-int main(int argc, char *argv[])
-{
-  
-    if (argc < 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
         fprintf(stderr, "usage: %s <file1> [file2] ...\n", argv[0]);
         return EXIT_FAILURE;
     }
-    for (int i = 1; i < argc; i++)
-    {
+    size_t chunk_size = 1024;
+
+    for (int i = 1; i < argc; i++) {
         int fd = open(argv[i], O_RDONLY);
 
-        if (fd == -1)
-        {
-        perror("open");
+        if (fd == -1) {
+            perror("open");
+            continue;
         }
-
-        if (i != 1){
+      
         printf("==> %s <==\n", argv[i]);
+
+        off_t file_size = lseek(fd, 0, SEEK_END);
+        off_t offset = 0;
+        char buffer[chunk_size];
+        int lines_count = 0;
+
+        for (offset = file_size - chunk_size; offset >= 0 && lines_count < 10; offset -= chunk_size) {
+            lseek(fd, offset, SEEK_SET);
+
+            ssize_t read_bytes = read(fd, buffer, chunk_size);
+
+            if (read_bytes == -1) {
+                perror("read");
+                close(fd);
+                return EXIT_FAILURE;
+            }
+
+            for (ssize_t i = read_bytes - 1; i >= 0 && lines_count < 10; i--) {
+                if (buffer[i] == '\n') {
+                    lines_count++;
+                }
+            }
         }
+      
+        lseek(fd, offset + chunk_size, SEEK_SET);
 
-        lseek(fd, -10 * sizeof(char), SEEK_END);
-
-        char buffer[1024];
-        int n;
-        while ((n = read(fd, buffer, sizeof(buffer))) > 0)
-        {
-            write(STDOUT_FILENO, buffer, n);
+        while (read(fd, buffer, sizeof(char)) > 0) {
+            write(STDOUT_FILENO, buffer, sizeof(char));
         }
 
         close(fd);
